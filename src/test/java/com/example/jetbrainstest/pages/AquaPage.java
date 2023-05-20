6649386a2d07d207ed02d49778ee6da18fdcf626
momanyshev <maksimmanyshev@yandex.ru>
2023-05-20 19:03:42 +0300

    Aqua tests added

---
+++ src/test/java/com/example/jetbrainstest/pages/AquaPage.java
@@ -0,0 +1,45 @@
+package com.example.jetbrainstest.pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class AquaPage {
+
+    WebDriver driver;
+
+    @FindBy(xpath = "//p[text()='JetBrains Aqua IDE']/preceding-sibling::a")
+    private WebElement downloadAquaButton;
+
+    @FindBy(xpath = "//a[@href='/']")
+    private WebElement mainPageButton;
+
+    @FindBy(xpath = "//button[text()='Plugin']")
+    private WebElement switcherButtonPlugin;
+
+    @FindBy(xpath = "//div[contains(@class,'navigation-buttons')]//a[text()='Get the plugin for your IDE']")
+    private WebElement switcherButtonPluginChild;
+
+    public Boolean downloadAquaButtonIsClickable(){
+        System.out.println("Проверка активности кнопки загрузки");
+        return downloadAquaButton.isEnabled();
+    }
+
+    public void mainPageButtonClick(){
+        mainPageButton.click();
+        System.out.println("Переход на главную страницу");
+    }
+
+    public String switcherButtonPluginClick(){
+        switcherButtonPlugin.click();
+        System.out.println("Проверка названия кнопки");
+        return switcherButtonPluginChild.getText();
+    }
+
+    public AquaPage(WebDriver driver) {
+        this.driver = driver;
+        PageFactory.initElements(driver, this);
+    }
+
+}
---
+++ src/test/java/com/example/jetbrainstest/tests/AquaTest.java
@@ -0,0 +1,41 @@
+package com.example.jetbrainstest.tests;
+
+import com.example.jetbrainstest.pages.AquaPage;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class AquaTest extends BaseTest {
+
+    private AquaPage aquaPage;
+    @BeforeEach
+    @Override
+    public void setUp(){
+        super.setUp();
+        getDriver().get("https://www.jetbrains.com/aqua/");
+        aquaPage = new AquaPage(getDriver());
+    }
+
+    @Test
+    @DisplayName("Проверка, что кнопка скачивания активна")
+    public void buttonCheck(){
+        assertTrue(aquaPage.downloadAquaButtonIsClickable());
+    }
+
+    @Test
+    @DisplayName("Проверка ссылки на активную страницу")
+    public void linkChecking(){
+        aquaPage.mainPageButtonClick();
+        assertEquals("https://www.jetbrains.com/", getDriver().getCurrentUrl(), "Открылась неверная сслыка");
+    }
+
+    @Test
+    @DisplayName("Проверка названия кнопки")
+    public void switcherButtonName(){
+        String btnName = "Get the plugin for your IDE";
+        assertEquals(btnName, aquaPage.switcherButtonPluginClick());
+    }
+}
