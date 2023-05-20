e8dcbf5f39ee1628078ccc9b573f1ad9fec48218
B0mb4rman <wrew234@gmail.com>
2023-05-20 21:48:26 +0200

    AppCode added

---
+++ src/test/java/com/example/jetbrainstest/tests/AppCodeTest.java
@@ -0,0 +1,27 @@
+package com.example.jetbrainstest.tests;
+
+import com.example.jetbrainstest.pages.AppCodePage;
+import com.example.jetbrainstest.pages.IntellijIdeaPage;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class AppCodeTest extends BaseTest {
+
+    private AppCodePage appCodePage;
+    @BeforeEach
+    @Override
+    public void setUp(){
+        super.setUp();
+        getDriver().get("https://www.jetbrains.com/objc/");
+        appCodePage = new AppCodePage(getDriver());
+    }
+
+    @Test
+    @DisplayName("Проверка, что кнопка скачивания активна")
+    public void buttonCheck(){
+        assertTrue(appCodePage.checkIfDownloadButtonIsClickable(), "Кнопка скачивания не активна");
+    }
+}
