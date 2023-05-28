a200dee898a17dbc0517f915a722df566b81a352
momanyshev <maksimmanyshev@yandex.ru>
2023-05-28 20:19:10 +0300

    fixed warnings

--- src/test/java/com/example/jetbrainstest/pages/AquaPage.java
+++ src/test/java/com/example/jetbrainstest/pages/AquaPage.java
@@ -1,12 +1,10 @@
 package com.example.jetbrainstest.pages;
 
 import com.example.jetbrainstest.AllureLogger;
-import io.qameta.allure.Step;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
-import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 //page_url = https://www.jetbrains.com/aqua/
