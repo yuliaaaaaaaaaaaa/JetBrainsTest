c785f8f0fc720294745322a5dc8dc1b1ed0b4c87
B0mb4rman <wrew234@gmail.com>
2023-06-03 17:14:15 +0200

    Added Allure and @Step to AppCodePage.java

--- src/test/java/com/example/jetbrainstest/pages/AppCodePage.java
+++ src/test/java/com/example/jetbrainstest/pages/AppCodePage.java
@@ -1,5 +1,6 @@
 package com.example.jetbrainstest.pages;
 
+import io.qameta.allure.Step;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -16,6 +17,7 @@ public class AppCodePage {
     @FindBy(css = "[href ='/objc/download/']")
     private WebElement downloadButton;
 
+    @Step("Проверка активности кнопки загрузки")
     public Boolean checkIfDownloadButtonIsClickable(){
         LOG.info("Проверка активности кнопки загрузки");
         return downloadButton.isEnabled();
