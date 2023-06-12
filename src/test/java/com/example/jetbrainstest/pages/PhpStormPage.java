b78d9a9aab1fee655a71439aa0b6c3ddb8a30003
maingrid <96063025+maingrid@users.noreply.github.com>
2023-06-12 18:06:25 +0300

    add allure-report to the project

--- src/test/java/com/example/jetbrainstest/pages/PhpStormPage.java
+++ src/test/java/com/example/jetbrainstest/pages/PhpStormPage.java
@@ -1,5 +1,6 @@
 package com.example.jetbrainstest.pages;
 
+import io.qameta.allure.Step;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -18,11 +19,13 @@ public class PhpStormPage {
     @FindBy(xpath = "//a[@href='/' and @data-test='site-logo']")
     public WebElement mainPageButton;
 
+    @Step("проверка нажатия кнопки на главную страницу")
     public void mainPagebuttonClick(){
         LOG.info("проверка нажатия кнопки на главную страницу");
         mainPageButton.click();
     }
 
+    @Step("проверка нажатия кнопки загрузить")
     public void ButtonDownloadPhpStormClick() {
         LOG.info("проверка нажатия кнопки загрузить");
         ButtonDownloadPhpStorm.click();
@@ -32,5 +35,4 @@ public class PhpStormPage {
         this.driver = driver;
         PageFactory.initElements(driver, this);
     }
-
 }
