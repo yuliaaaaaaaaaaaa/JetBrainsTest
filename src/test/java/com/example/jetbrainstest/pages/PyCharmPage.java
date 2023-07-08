426ae587b8654b7029fdd69bcefbfd31bdb5f22a
Butsko1 <ant.butsko@gmail.com>
2023-07-08 23:16:43 +0300

    Подключил кастомный логгер v.2

--- src/test/java/com/example/jetbrainstest/pages/PyCharmPage.java
+++ src/test/java/com/example/jetbrainstest/pages/PyCharmPage.java
@@ -1,12 +1,12 @@
 package com.example.jetbrainstest.pages;
 
 import com.example.jetbrainstest.AllureLogger;
-import io.qameta.allure.Step;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
-import org.slf4j.Logger;
+
 import org.slf4j.LoggerFactory;
 
 public class PyCharmPage {
