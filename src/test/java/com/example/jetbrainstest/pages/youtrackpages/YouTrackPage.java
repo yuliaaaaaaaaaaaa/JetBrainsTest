7fad9dc1d06ba7c18f37a4a09d59c8dce5244db3
Igor <SpaceTuna@vk.com>
2023-11-18 15:11:47 +0300

    Allure logger added

--- src/test/java/com/example/jetbrainstest/pages/youtrackpages/YouTrackPage.java
+++ src/test/java/com/example/jetbrainstest/pages/youtrackpages/YouTrackPage.java
@@ -1,18 +1,17 @@
 package com.example.jetbrainstest.pages.youtrackpages;
 
 
-import io.qameta.allure.Step;
+import com.example.jetbrainstest.AllureLogger;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
-import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 // https://www.jetbrains.com/youtrack/
 public class YouTrackPage {
 
-    private final Logger LOG = LoggerFactory.getLogger(YouTrackPage.class);
+    private final AllureLogger LOG = new AllureLogger(LoggerFactory.getLogger(YouTrackPage.class));
     WebDriver driver;
 
     @FindBy(xpath = "//a[@href='/youtrack/whatsnew/']")
@@ -36,43 +35,36 @@ public class YouTrackPage {
     @FindBy(xpath = "//a[@href='/youtrack/buy/']")
     private WebElement PricingButton;
 
-    @Step("Проверка кликабельности кнопки What's New")
     public Boolean checkIfWhatsNewButtonIsClickable(){
         LOG.info("Проверка кликабельности кнопки What's New");
         return  WhatsNewButton.isEnabled();
     }
 
-    @Step("Проверка кликабельности кнопки Agile")
     public Boolean checkIfAgileButtonIsClickable(){
         LOG.info("Проверка кликабельности кнопки Agile");
         return  AgileButton.isEnabled();
     }
 
-    @Step("Проверка кликабельности кнопки Teams")
     public Boolean checkIfTeamsButtonIsClickable(){
         LOG.info("Проверка кликабельности кнопки Teams");
         return  TeamsButton.isEnabled();
     }
 
-    @Step("Проверка кликабельности кнопки Features")
     public Boolean checkIfFeaturesButtonIsClickable(){
         LOG.info("Проверка кликабельности кнопки Features");
         return  FeaturesButton.isEnabled();
     }
 
-    @Step("Проверка кликабельности кнопки Helpdesk")
     public Boolean checkIfHelpdeskButtonIsClickable(){
         LOG.info("Проверка кликабельности кнопки Helpdesk");
         return  HelpdeskButton.isEnabled();
     }
 
-    @Step("Проверка кликабельности кнопки Support")
     public Boolean checkIfSupportButtonIsClickable(){
         LOG.info("Проверка кликабельности кнопки Support");
         return  SupportButton.isEnabled();
     }
 
-    @Step("Проверка кликабельности кнопки Pricing")
     public Boolean checkIfPricingButtonIsClickable(){
         LOG.info("Проверка кликабельности кнопки Pricing");
         return  PricingButton.isEnabled();
