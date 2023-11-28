f7fc1a20a9c50d32cc7597706bbf9d0afed4cc1a
DianaBlah <dianablah2@gmail.com>
2023-11-28 13:28:17 +0300

    added QodanaPage and QodanaTest

---
+++ src/test/java/com/example/jetbrainstest/pages/qodanapages/QodanaPage.java
@@ -0,0 +1,74 @@
+package com.example.jetbrainstest.pages.qodanapages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+// https://www.jetbrains.com/qodana
+
+public class QodanaPage {
+    private final Logger LOG = LoggerFactory.getLogger(QodanaPage.class);
+    WebDriver driver;
+
+    @FindBy(xpath = "//a[@href='https://www.jetbrains.com/help/qodana/getting-started.html']")
+    private WebElement DocumentationButton;
+
+    @FindBy(xpath = "//a[contains(@class,'wt-col-inline wt-text-2 menu-second__link wt-text-2_theme_dark _theme-dark wt-text-2_hardness_average menu-item') and text()='Blog']")
+    private WebElement BlogButton;
+
+    @FindBy(xpath = "//a[@href='/qodana/request-a-demo/']")
+    private WebElement RequestDemoButton;
+
+    @FindBy(xpath = "//a[@href='/qodana/buy/']")
+    private WebElement PricingButton;
+
+    @FindBy(xpath = "//a[contains(@class,'wt-col-inline menu-second__download-button wt-button wt-button_size_s wt-button_mode_primary wt-button_theme_dark') and text()='Try for free']")
+    private WebElement TryForFreeButton1;
+
+    @FindBy(xpath = "//a[contains(@class,'_main_1o2xc3v_17 _modeClassic_1o2xc3v_168 _sizeL_1o2xc3v_106 _alignIconLeft_1o2xc3v_77 _dark_1o2xc3v_62 qodana-button qodana-button_header')]")
+    private WebElement TryForFreeButton2;
+
+    public Boolean checkIfDocumentationButtonIsClickable() {
+        LOG.info("Проверка кликабельности кнопки Documentation");
+        return DocumentationButton.isEnabled();
+    }
+
+    public Boolean checkIfBlogButtonIsClickable(){
+        LOG.info("Проверка кликабельности кнопки Blog");
+        return  BlogButton.isEnabled();
+    }
+
+    public Boolean checkIfRequestDemoButtonIsClickable(){
+        LOG.info("Проверка кликабельности кнопки Request Demo");
+        return  RequestDemoButton.isEnabled();
+    }
+
+    public void requestDemoButtonClick(){
+        RequestDemoButton.click();
+        LOG.info("Переход на страницу Request a demo");
+    }
+
+    public Boolean checkIfPricingButtonIsClickable(){
+        LOG.info("Проверка кликабельности кнопки Pricing");
+        return  PricingButton.isEnabled();
+    }
+
+    public Boolean checkIfTryForFreeButton1IsClickable(){
+        LOG.info("Проверка кликабельности кнопки Try For Free в меню");
+        return  TryForFreeButton1.isEnabled();
+    }
+
+    public Boolean checkIfTryForFreeButton2IsClickable(){
+        LOG.info("Проверка кликабельности кнопки Try For Free на странице");
+        return  TryForFreeButton2.isEnabled();
+    }
+
+    public QodanaPage(WebDriver driver){
+        this.driver = driver;
+        PageFactory.initElements(driver, this);
+    }
+}
+
---
+++ src/test/java/com/example/jetbrainstest/tests/QodanaTest.java
@@ -0,0 +1,71 @@
+package com.example.jetbrainstest.tests;
+
+import com.example.jetbrainstest.pages.qodanapages.QodanaPage;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class QodanaTest extends BaseTest{
+    private QodanaPage QoPage;
+
+    @BeforeEach
+    @Override
+    public void setUp(){
+        super.setUp();
+        getDriver().get("https://www.jetbrains.com/qodana/");
+        QoPage = new QodanaPage(getDriver());
+        System.out.println("Начало проверки:");
+    }
+    @AfterEach
+    public void tearDown(){
+        super.tearDown();
+        System.out.println("Проверка окончена");
+    }
+
+    @Test
+    @DisplayName("Проверка активности кнопки Documentation")
+    public void  DocumentationButtonTest(){
+        assertTrue(QoPage.checkIfDocumentationButtonIsClickable(),"Кнопка Documentation неактивна");
+    }
+
+    @Test
+    @DisplayName("Проверка активности кнопки Blog")
+    public void  BlogButtonTest(){
+        assertTrue(QoPage.checkIfBlogButtonIsClickable(),"Кнопка Blog неактивна");
+    }
+
+    @Test
+    @DisplayName("Проверка активности кнопки Request Demo")
+    public void  RequestDemoButtonTest(){
+        assertTrue(QoPage.checkIfRequestDemoButtonIsClickable(),"Кнопка Request Demo неактивна");
+    }
+
+    @Test
+    @DisplayName("Проверка активности кнопки Pricing")
+    public void  PricingButtonTest(){
+        assertTrue(QoPage.checkIfPricingButtonIsClickable(),"Кнопка Pricing неактивна");
+    }
+
+    @Test
+    @DisplayName("Проверка кликабельности кнопки Try For Free в меню")
+    public void  checkIfTryForFreeButton1IsClickable(){
+        assertTrue(QoPage.checkIfTryForFreeButton1IsClickable(),"Кнопка Try For Free неактивна");
+    }
+
+    @Test
+    @DisplayName("Проверка кликабельности кнопки Try For Free на странице")
+    public void  checkIfTryForFreeButton2IsClickable(){
+        assertTrue(QoPage.checkIfTryForFreeButton2IsClickable(),"Кнопка Try For Free неактивна");
+    }
+
+    @Test
+    @DisplayName("Переход на страницу Request a demo")
+    public void RequestDemoButtonClickTest(){
+        QoPage.requestDemoButtonClick();
+        assertEquals("https://www.jetbrains.com/qodana/request-a-demo/", getDriver().getCurrentUrl(), "Переход на страницу Request a demo не осуществлен");
+    }
+}
