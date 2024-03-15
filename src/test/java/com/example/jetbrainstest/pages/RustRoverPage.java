c35be41ccf5e18d265d7f6d06ebba6144cb928a4
Ilya Poplavsky <i.poplavsky@paynocchio.com>
2024-03-15 15:18:30 +0300

    SLF4J Logger implemented to project

--- src/test/java/com/example/jetbrainstest/pages/RustRoverPage.java
+++ src/test/java/com/example/jetbrainstest/pages/RustRoverPage.java
@@ -4,14 +4,19 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 //https://www.jetbrains.com/rust/
 public class RustRoverPage
 {
+    private final Logger LOG = LoggerFactory.getLogger(RustRoverPage.class);
     WebDriver driver;
     @FindBy(css = "a[href='/rust/download/']")
     private WebElement downloadRustPreview;
 
     public String clickRustDownloadAndCheckUrl(){
+        LOG.info("Checking if URL changed after button has been clicked");
         driver.get("https://www.jetbrains.com/rust/");
         downloadRustPreview.click();
         return driver.getCurrentUrl();
