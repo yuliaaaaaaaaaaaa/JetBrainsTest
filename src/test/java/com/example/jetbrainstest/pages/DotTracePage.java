9da84e0b22c4b6e6dd16ffb46b2aadabd2626f82
yuliaaaaaaaaaaaa <yuliamlfv@gmail.com>
2024-06-12 15:18:47 +0300

    dotTrace commil

---
+++ src/test/java/com/example/jetbrainstest/pages/DotTracePage.java
@@ -0,0 +1,58 @@
+package com.example.jetbrainstest.pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class DotTracePage {
+    WebDriver driver;
+
+    @FindBy(xpath = "//a[@href='/profiler/download/']")
+    private WebElement DownloadButton;
+
+    @FindBy(xpath = "//*[@id=\"js-menu-second-desktop\"]/a[7]")
+    private WebElement PricingButton;
+
+    @FindBy(xpath = "//div[contains(@class, 'wt-col-sm-12')][.//a[contains(@class, 'header-section__button')]]")
+    private WebElement CheckOutOurMemoryProfilerButton;
+
+    @FindBy(xpath = "//div/a[@href=\"/dotnet\"]")
+    private WebElement PartOfDotUltimateElement;
+
+    @FindBy(xpath = "//h2[@class='rs-h2 rs-h2_theme_dark wt-offset-top-96']")
+    private WebElement OptimizeNETApplications;
+
+    @FindBy(xpath = "//a[@href='/profiler/features']")
+    private WebElement MoreFeatures;
+
+    public Boolean downloadButtonIsClickable() {
+        System.out.println("Проверка активности кнопки Download");
+        return DownloadButton.isEnabled();
+    }
+    public Boolean pricingButtonIsClickable() {
+        System.out.println("Проверка активности кнопки Pricing");
+        return PricingButton.isEnabled();
+    }
+    public Boolean checkOutOurMemoryProfilerButtonIsClickable() {
+        System.out.println("Проверка активности кнопки Check out our memory profiler");
+        return CheckOutOurMemoryProfilerButton.isEnabled();
+    }
+    public Boolean partofdotUltimateElementIsDisplayed() {
+        System.out.println("Проверка, что элемент Part of dotUltimate есть на странице");
+        return PartOfDotUltimateElement.isDisplayed();
+    }
+    public Boolean optimizeNETApplicationsIsDisplayed() {
+        System.out.println("Проверка, что элемент Optimize.NETApplications есть на странице");
+        return OptimizeNETApplications.isDisplayed();
+    }
+    public String MoreFeaturesButtonClick() {
+        System.out.println("Нажатие на кнопку More Features");
+        MoreFeatures.click();
+        return driver.getCurrentUrl();
+    }
+    public DotTracePage(WebDriver driver) {
+        this.driver = driver;
+        PageFactory.initElements(driver, this);
+    }
+}
---
+++ src/test/java/com/example/jetbrainstest/tests/DotTraceTest.java
@@ -0,0 +1,60 @@
+package com.example.jetbrainstest.tests;
+
+import com.example.jetbrainstest.pages.DotTracePage;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class DotTraceTest extends BaseTest{
+
+    private DotTracePage DotTracePage;
+
+    @BeforeEach
+    @Override
+    public void setUp() {
+        super.setUp();
+        getDriver().get("https://www.jetbrains.com/profiler/");
+        DotTracePage = new DotTracePage(getDriver());
+    }
+    @AfterEach
+    public void tearDown(){
+        super.tearDown();
+    }
+
+    @Test
+    @DisplayName("Проверка активности кнопки Download")
+    public void downloadButtonTest() {
+        assertTrue(DotTracePage.downloadButtonIsClickable(),"Кнопка Download не активна");
+    }
+    @Test
+    @DisplayName("Проверка активности кнопки Pricing")
+    public void pricingButtonTest() {
+        assertTrue(DotTracePage.pricingButtonIsClickable(),"Кнопка Pricing не активна");
+    }
+    @Test
+    @DisplayName("Проверка активности кнопки Check out our memory profiler")
+    public void checkOutOurMemoryProfilerTest() {
+        assertTrue(DotTracePage.checkOutOurMemoryProfilerButtonIsClickable(),"Кнопка Check out our memory profiler не активна");
+    }
+    @Test
+    @DisplayName("Проверка, что элемент Part of dotUltimate есть на странице")
+    public void partofdotUltimateTest() {
+        assertTrue(DotTracePage.partofdotUltimateElementIsDisplayed(),"Элемента Part of dotUltimate нет на странице");
+    }
+    @Test
+    @DisplayName("Проверка, что элемент Optimize.NETApplications есть на странице")
+    public void optimizeNETApplicationsTest() {
+        assertTrue(DotTracePage.optimizeNETApplicationsIsDisplayed(),"Элемента Optimize.NETApplications нет на странице");
+    }
+    @Test
+    @DisplayName("Переход в раздел Features по кнопке More Features")
+    public void moreFeaturesTest() {
+        String expectedUrl = "https://www.jetbrains.com/profiler/features/";
+        String actualUrl = DotTracePage.MoreFeaturesButtonClick();
+        assertEquals(expectedUrl,actualUrl,"Переход не на ту страницу");
+    }
+}
